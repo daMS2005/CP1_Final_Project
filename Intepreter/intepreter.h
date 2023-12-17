1c0e8392e96f3055a9565f21d9eecbfd1d567756
trinirocaaa <136647006+trinirocaaa@users.noreply.github.com>
2023-12-17 19:05:29 +0100

    Add files via upload

--- Intepreter/intepreter.h
+++ Intepreter/intepreter.h
@@ -1,22 +1,37 @@
-
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
-#include "parser.h"  // Include your parser header file
+#include "../PARSER/parser.h"
 
-// Define the structure for the interpreter state
+// Enumeration for the different types of nodes in the AST
+typedef enum {
+    NODE_TYPE_FUNCTION_CALL,             // Represents a function call
+    NODE_TYPE_VARIABLE_ASSIGNMENT,       // Represents variable assignment
+    NODE_TYPE_IF_STATEMENT,              // Represents an if statement
+    NODE_TYPE_LOOP,                      // Represents a loop
+    // ... add more node types as needed for your AST
+} NodeType;
+
+// Structure for representing nodes in the AST
+typedef struct ASTNode {
+    NodeType type;            // The type of the node
+    char* functionName;       // Function name (for function calls)
+    char* arguments;          // Arguments to the function (if any)
+    struct ASTNode* left;     // Pointer to the left child (if applicable)
+    struct ASTNode* right;    // Pointer to the right child (if applicable)
+    // ... add other fields as needed for your AST
+} ASTNode;
+
+// Structure for maintaining the state of the interpreter
 typedef struct {
     // Add fields relevant to the interpreter's state
-    // This might include a symbol table, a pointer to the AST, etc.
+    // For example, a symbol table, a pointer to the AST, etc.
 } InterpreterState;
 
-// Function to interpret the AST
-void interpretAST(InterpreterState* state, ASTNode* root);
-
-// Additional functions for interpreting specific types of nodes
-// For example:
-void interpretExpression(InterpreterState* state, ASTNode* node);
-void interpretStatement(InterpreterState* state, ASTNode* node);
-// Add more as needed
+// Function prototypes for interpreting the AST and its nodes
+void interpretAST(InterpreterState* state, ASTNode* root);                    // Interprets the entire AST
+void interpretExpression(InterpreterState* state, ASTNode* node);             // Interprets an expression node
+void interpretStatement(InterpreterState* state, ASTNode* node);              // Interprets a statement node
+// ... add more function prototypes as needed
 
 #endif // INTERPRETER_H
--- PARSER/parser.h
+++ PARSER/parser.h
@@ -1,19 +1,23 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdio.h>
+#include <string.h>
+
 // Define the different types of tokens
 typedef enum {
     TOKEN_IDENTIFIER, 
     TOKEN_KEYWORD, 
     TOKEN_LITERAL, 
-    TOKEN_OPERATOR
+    TOKEN_OPERATOR,
+    TOKEN_EOF  // End of file/input token
     // Add other token types as needed
 } TokenType;
 
 // Structure for a token
 typedef struct {
     TokenType type;
-    char* value;
+    char* value; // Ensure proper memory management for these strings
 } Token;
 
 // Parser state structure
@@ -22,5 +26,12 @@ typedef struct {
     Token lookaheadToken;
     // Add other state information as needed
 } ParserState;
+
+// Function declarations for parser operations (implement these in parser.c)
+void getNextToken(ParserState* state);
+void parseProgram(ParserState* state);
+void parseStatement(ParserState* state);
+void parseExpression(ParserState* state);
+void parseFunctionCall(ParserState* state);
 
 #endif // PARSER_H
